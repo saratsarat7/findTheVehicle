84ff51463ec1173d1e61abfd0b21be5bcd3b3561
Sarat Chandra Prabhu Betha <saratbetha@in.ibm.com>
2021-04-03 03:01:34 +0530

    Merged all into a single dart file

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,12 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 import 'package:flutter/material.dart';
 
-import 'UserImagePicker.dart';
+import 'package:image_picker/image_picker.dart';
 
 void main() => runApp(MaterialApp(
-  debugShowCheckedModeBanner: false,
-  home: HomeScreen(),
-));
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    ));
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -21,8 +21,15 @@ class _HomeScreenState extends State<HomeScreen> {
   File _userImageFile;
   var result = "";
 
-  void _pickedImage(File image) {
-    _userImageFile = image;
+  ImagePicker picker = ImagePicker();
+
+  void _pickImage(ImageSource imageSource) async {
+    final pickedImageFile = await picker.getImage(
+      source: imageSource,
+    );
+    setState(() {
+      _userImageFile = File(pickedImageFile.path);
+    });
   }
 
   //recognise_Text
@@ -52,10 +59,49 @@ class _HomeScreenState extends State<HomeScreen> {
         body: Container(
           child: Center(
             child: Column(
-              children: [
-                UserImagePicker(_pickedImage),
+              children: <Widget>[
                 Padding(
-                    padding: const EdgeInsets.all(16.0), child: Text(result)),
+                  padding: const EdgeInsets.all(18.0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Container(
+                      color: Colors.orangeAccent.withOpacity(0.3),
+                      width: MediaQuery.of(context).size.width,
+                      height: 400,
+                      child: _userImageFile != null
+                          ? GestureDetector(
+                              onTap: () {
+                                _pickImage(ImageSource.camera);
+                              },
+                              child: Image(
+                                image: FileImage(_userImageFile),
+                              ),
+                            )
+                          : GestureDetector(
+                              onTap: () {
+                                _pickImage(ImageSource.camera);
+                              },
+                              child: Center(
+                                child: Text("Number Plate"),
+                              ),
+                            ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                TextButton.icon(
+                  onPressed: () {
+                    recogniseText();
+                  },
+                  icon: Icon(Icons.image_search),
+                  label: Text(
+                    'Get Details',
+                  ),
+                ),
+                Padding(
+                    padding: const EdgeInsets.all(10.0), child: Text(result)),
               ],
             ),
           ),
